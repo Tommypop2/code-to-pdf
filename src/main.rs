0c4596d28be73fe92665ebb1831db1cd7999f72e
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-03-03 22:37:55 +0000

    feat: walk a directory and print each file to a separate page

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@ use printpdf::*;
 use std::collections::BTreeMap;
 use std::fs;
 use std::io::BufRead;
+use std::path::{Path, PathBuf};
 use syntect::easy::HighlightFile;
 use syntect::highlighting::{Color, Highlighter, Style, ThemeSet};
 use syntect::html::highlighted_html_for_file;
 use syntect::parsing::SyntaxSet;
 use syntect::util::as_24_bit_terminal_escaped;
 mod process_file;
+use ignore::Walk;
 use process_file::process_file;
-fn new_page_contents(page_dimensions: (f32, f32), font_id: FontId) -> Vec<Op> {
+fn new_page_contents(page_dimensions: (f32, f32), font_id: FontId, path: PathBuf) -> Vec<Op> {
     vec![
         Op::SetLineHeight { lh: Pt(14.0) },
         // Write metadata
@@ -24,7 +26,7 @@ fn new_page_contents(page_dimensions: (f32, f32), font_id: FontId) -> Vec<Op> {
             },
         },
         Op::WriteText {
-            items: vec![TextItem::Text("src/hello.js".to_owned())],
+            items: vec![TextItem::Text(path.to_str().unwrap().to_owned())],
             size: Pt(12.0),
             font: font_id,
         },
@@ -50,20 +52,44 @@ fn main() {
     //     return;
     // }
     let page_dimensions: (f32, f32) = (210.0, 297.0);
-    let mut doc = PdfDocument::new("My first PDF");
+    let mut doc = PdfDocument::new("Project Code");
     let helvetica_bytes = include_bytes!("../fonts/Helvetica.ttf");
     let font = ParsedFont::from_bytes(helvetica_bytes, 33, &mut vec![]).unwrap();
     let font_id = doc.add_font(&font);
     // Highlighting stuff
     let ss = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
+    let mut pages: Vec<PdfPage> = vec![];
+    for result in Walk::new("./test_folder") {
+        match result {
+            Ok(entry) => {
+                if entry.file_type().is_some_and(|f| f.is_file()) {
+                    // dbg!(entry.path());
+                    let mut page_contents = new_page_contents(
+                        page_dimensions,
+                        font_id.clone(),
+                        entry.path().to_path_buf(),
+                    );
+                    process_file(
+                        &ss,
+                        &ts,
+                        font_id.clone(),
+                        &mut page_contents,
+                        entry.path().to_path_buf(),
+                    );
+                    pages.push(PdfPage::new(
+                        Mm(page_dimensions.0),
+                        Mm(page_dimensions.1),
+                        page_contents,
+                    ));
+                }
+            }
+            Err(err) => println!("ERROR: {}", err),
+        }
+    }
 
-    let mut page_contents = new_page_contents(page_dimensions, font_id.clone());
-    process_file(&ss, &ts, font_id, &mut page_contents, "./test_folder/hello.js".into());
-		
-    let page1 = PdfPage::new(Mm(page_dimensions.0), Mm(page_dimensions.1), page_contents);
     let pdf_bytes: Vec<u8> = doc
-        .with_pages(vec![page1])
+        .with_pages(pages)
         .save(&PdfSaveOptions::default(), &mut vec![]);
     fs::write("./hello.pdf", pdf_bytes).unwrap();
 }

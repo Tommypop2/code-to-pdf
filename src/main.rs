d8a47ee5cfd11f316957222eab5d4b3a22357bd5
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-03-03 20:26:16 +0000

    attempted to print coloured strings to the pdf file

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,34 @@ fn main() {
     //     println!("Please pass in a file to highlight");
     //     return;
     // }
-
+    let page_dimensions: (f32, f32) = (210.0, 297.0);
+    let mut doc = PdfDocument::new("My first PDF");
+    let helvetica_bytes = include_bytes!("../fonts/Helvetica.ttf");
+    let font = ParsedFont::from_bytes(helvetica_bytes, 33, &mut vec![]).unwrap();
+    let font_id = doc.add_font(&font);
+    let text_pos = Point {
+        x: Mm(10.0).into(),
+        y: Mm(page_dimensions.1 - 20.0).into(),
+    }; // from bottom left
+       // Highlighting stuff
     let ss = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
 
     let mut highlighter =
         HighlightFile::new("./hello.js", &ss, &ts.themes["InspiredGitHub"]).unwrap();
-    let mut lines= String::new();
+    let mut page_contents = vec![
+        Op::SetLineHeight { lh: Pt(33.0) },
+        // Op::SetWordSpacing { pt: Pt(1000.0) },
+        // Op::SetCharacterSpacing { multiplier: 10.0 },
+        Op::SetTextCursor { pos: text_pos },
+        Op::SetFillColor {
+            col: color::Color::Rgb(Rgb { r: 255.0, g: 0.0, b: 0.0, icc_profile: None }),
+        },
+				Op::SetOutlineColor {
+					col: color::Color::Rgb(Rgb { r: 255.0, g: 0.0, b: 0.0, icc_profile: None }),
+			},
+    ];
+    let mut lines = String::new();
     let mut line = String::new();
     while highlighter.reader.read_line(&mut line).unwrap() > 0 {
         {
@@ -30,36 +51,24 @@ fn main() {
                 .highlight_lines
                 .highlight_line(&line, &ss)
                 .unwrap();
-						dbg!(&regions);
-            lines += &format!("{}", as_24_bit_terminal_escaped(&regions[..], true));
-
+            for r in regions {
+                let (style, text) = r;
+                page_contents.push(Op::WriteText {
+                    items: vec![TextItem::Text(text.to_owned())],
+                    size: Pt(12.0),
+                    font: font_id.clone(),
+                });
+            }
+            page_contents.push(Op::AddLineBreak)
+            // lines += &format!("{}", as_24_bit_terminal_escaped(&regions[..], true));
         } // until NLL this scope is needed so we can clear the buffer after
         line.clear(); // read_line appends so we need to clear between lines
     }
-		print!("{}", &lines);
+    print!("{}", &lines);
     // Clear the formatting
     println!("\x1b[0m");
-    let page_dimensions: (f32, f32) = (210.0, 297.0);
-    let mut doc = PdfDocument::new("My first PDF");
-    let roboto_bytes = include_bytes!("../fonts/Helvetica.ttf");
-    let font = ParsedFont::from_bytes(roboto_bytes, 33, &mut vec![]).unwrap();
-    let font_id = doc.add_font(&font);
-    let text_pos = Point {
-        x: Mm(10.0).into(),
-        y: Mm(page_dimensions.1 - 20.0).into(),
-    }; // from bottom left
-    let page1_contents = vec![
-        Op::SetLineHeight { lh: Pt(33.0) },
-        // Op::SetWordSpacing { pt: Pt(1000.0) },
-        // Op::SetCharacterSpacing { multiplier: 10.0 },
-        Op::SetTextCursor { pos: text_pos },
-        Op::WriteText {
-            items: vec![TextItem::Text(lines)],
-            size: Pt(33.0),
-            font: font_id,
-        },
-    ];
-    let page1 = PdfPage::new(Mm(page_dimensions.0), Mm(page_dimensions.1), page1_contents);
+
+    let page1 = PdfPage::new(Mm(page_dimensions.0), Mm(page_dimensions.1), page_contents);
     let pdf_bytes: Vec<u8> = doc
         .with_pages(vec![page1])
         .save(&PdfSaveOptions::default(), &mut vec![]);

784ba6bec5beb4e70e341364d25da05f5f1ed1a7
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-04-24 21:39:51 +0100

    feat: add initial GUI implementation

---
+++ crates/c2pdf-gui/src/main.rs
@@ -0,0 +1,112 @@
+use std::{
+  path::PathBuf,
+  sync::{Arc, Mutex, atomic::AtomicI32, mpsc},
+  thread::{self, sleep},
+  time::Duration,
+};
+
+use floem::{
+  action::open_file,
+  ext_event::create_signal_from_channel,
+  prelude::*,
+  reactive::{Scope, SignalRead, SignalWrite, create_effect},
+};
+use tokio::runtime::Runtime;
+
+fn main() {
+  let runtime = Runtime::new().expect("Couldn't start runtime");
+
+  // We must make it so that the main task is under the tokio runtime so that APIs like
+  // tokio::spawn work
+  runtime.block_on(async { tokio::task::block_in_place(|| floem::launch(app_view)) })
+}
+
+fn app_view() -> impl IntoView {
+  let w = floem::file::FileDialogOptions::new().select_directories();
+  let cx = Scope::current();
+  let (read, write) = cx.create_signal(2);
+  let (dir_path, set_dir_path) = cx.create_signal::<Option<PathBuf>>(None);
+  let (tx, rx): (
+    crossbeam_channel::Sender<i32>,
+    crossbeam_channel::Receiver<i32>,
+  ) = crossbeam_channel::unbounded();
+  let status = create_signal_from_channel(rx);
+  let num = Arc::new(Mutex::new(0));
+  let thread_num = num.clone();
+  let thread_tx = tx.clone();
+  let increment_tx = tx.clone();
+  let t = thread::spawn(move || {
+    loop {
+      sleep(Duration::from_millis(1000));
+      let x = thread_num
+        .lock()
+        .and_then(|mut x| {
+          *x += 1;
+          Ok(*x)
+        })
+        .unwrap();
+      thread_tx.send(x).unwrap();
+    }
+  });
+  create_effect(move |_| {
+    println!(
+      "{}",
+      dir_path
+        .get()
+        .and_then(|path| path.to_str().map(|x| x.to_string()))
+        .unwrap_or("".to_string())
+    );
+  });
+  let increment_num = num.clone();
+  let decrement_num = num.clone();
+  v_stack((
+    button("Open File Picker").action(move || {
+      open_file(w.clone(), move |f| {
+        if let Some(file) = f {
+          let path = file.path[0].clone();
+          set_dir_path.set(Some(path));
+        };
+      });
+    }),
+    label(move || {
+      format!(
+        "Path {}",
+        dir_path
+          .get()
+          .and_then(|path| path.to_str().map(|x| x.to_string()))
+          .unwrap_or("".to_string())
+      )
+    }),
+		button("Process!").action(move || {
+			// Here we need to actually invoke `code-to-pdf`
+		}),
+    h_stack((
+      button("Increment").action(move || {
+        let x = increment_num
+          .clone()
+          .lock()
+          .and_then(|mut x| {
+            *x += 1;
+            Ok(*x)
+          })
+          .unwrap();
+        tx.send(x);
+      }),
+      label(move || format!("Value: {}", status.get().unwrap_or(0))),
+      button("Decrement").action(move || {
+        let x = decrement_num
+          .clone()
+          .lock()
+          .and_then(|mut x| {
+            *x -= 1;
+            Ok(*x)
+          })
+          .unwrap();
+
+        increment_tx.send(x);
+      }),
+    ))
+    .style(|s| s.size_full().items_center().justify_center().gap(10)),
+  ))
+  .style(|s| s.size_full().items_center().justify_center().gap(10))
+}
